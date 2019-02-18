a51723b9cd2a3ad7d1dc8ebda7573d7ef154ad89
Антон Мещеряков <anton23.pochta@gmail.com>
2019-02-19 01:12:47 +0500

    выполнил часть превого урока, прописывал переменные

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+const cartWrapper = document.querySelector('.cart__wrapper'),
+      cart = document.querySelector('.cart'),
+      close = document.querySelector('.card__close'),
+      open = document.querySelector('#cart'),
+      goodsBtn = document.querySelectorAll('.goods__btn'),
+      products = document.querySelectorAll('.goods__item'),
+      confirn = document.querySelector('.confirn'),
+      badge = document.querySelector('.nav__badge')
+
+
+
+      
